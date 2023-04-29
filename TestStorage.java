3c7880f77e6c06d82ee9712e75e3673b4d159832
maksmolchdmitr <maksmolchdmitr@gmail.com>
2023-04-29 23:51:48 +0300

    add tests

--- TestStorage.java
+++ TestStorage.java
@@ -6,6 +6,39 @@ public final class TestStorage {
     public final static List<TestData> tests = new ArrayList<>(
             List.of(
                     new TestData("""
+                            2
+                            2 1 7 10
+                            4 4 10 8
+                            7
+                            2 1
+                            4 3
+                            4 4
+                            5 5
+                            7 8
+                            9 7
+                            6 10
+                            """, "1 1 2 2 0 1 0 "),
+                    new TestData("""
+                            2
+                            2 1 4 4
+                            3 2 6 6
+                            5
+                            2 1
+                            3 2
+                            4 3
+                            3 3
+                            4 1
+                            """, "1 2 1 2 0 "),
+                    new TestData("""
+                            1
+                            1 1 5 5
+                            4
+                            1 1
+                            1 5
+                            5 1
+                            5 5
+                            """, "1 0 0 0 "),
+                    new TestData("""
                             4
                             2 2 6 8
                             5 4 9 10
